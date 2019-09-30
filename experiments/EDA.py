cf4136f361b8098b7e67c10cf5b870b6d7d6ee76
M1F1 <filek.michal@gmail.com>
2019-09-30 19:55:03 +0200

    add train script with in-sample evaluation

--- experiments/EDA.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import os
-import sys
-from settings import PROJECT_ROOT
-filename = 'file_test.csv'
-data_path = os.path.join(PROJECT_ROOT, 'data', filename)
-df = pd.read_csv(data_path, index_col=0)
